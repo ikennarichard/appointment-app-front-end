ea43152dce6f41936f1b65d24b2ec1a614060a3a
theabroadwolf <belalgamal00@gmail.com>
2023-08-21 14:46:00 +0300

    Update react router and add layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import Layout from './components/Layout/layout';
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <div>Main page</div>,
+      },
+      {
+        path: 'reservations',
+        element: <div>Reservations</div>,
+      },
+      {
+        path: 'newReservation',
+        element: <div>Create reservation</div>,
+      },
+      {
+        path: 'reservations/:id',
+        element: <div>Rese</div>,
+      },
+      {
+        path: 'newCar',
+        element: <div>Create new car</div>,
+      },
+      {
+        path: 'deleteCar',
+        element: <div>Delete car</div>,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
