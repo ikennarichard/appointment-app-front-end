d2f26d8cc44e2ab7ca951b01ccb8080ab5536e70
theabroadwolf <belalgamal00@gmail.com>
2023-08-23 01:50:47 +0300

    Add style for login page

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -2,27 +2,25 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from '../navbar/navbar';
-import SignIn from '../pages/sign_in';
+import SignIn from '../pages/signIn/sign_in';
 
 function Layout() {
   const resourceOwner = useSelector((state) => state.auth.resource_owner);
 
-  return (
-    <div>
-      { resourceOwner
-        ? (
-          <div className="container-fluid h-100">
-            <div className="row h-100">
-              <Navbar />
-              <main className="col-12 col-lg-9 col-md-7">
-                <Outlet />
-              </main>
-            </div>
-          </div>
-        )
-        : <SignIn />}
-    </div>
-  );
+  if (resourceOwner) {
+    return (
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <Navbar />
+          <main className="col-12 col-lg-9 col-md-7">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    );
+  }
+
+  return <SignIn />;
 }
 
 export default Layout;
