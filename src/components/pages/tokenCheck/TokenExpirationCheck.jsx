f4a279a7aa4a4c1d505ed0b8856a1ebdecb73e2d
mnaibei <muchajulius@gmail.com>
2023-08-28 11:32:30 +0300

    Linters fix

--- src/components/pages/tokenCheck/TokenExpirationCheck.jsx
+++ src/components/pages/tokenCheck/TokenExpirationCheck.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { resetTokens } from "../../../redux/auth/authSlice";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { resetTokens } from '../../../redux/auth/authSlice';
 
 const TokenExpirationCheck = () => {
   const dispatch = useDispatch();
-  //function to auto-log out user when token expires
+  // function to auto-log out user when token expires
   useEffect(() => {
     const accessToken = localStorage.getItem('access_token');
     const expiryTime = localStorage.getItem('expires_in');
-    let notice = "session expired, please log in again";
+    const notice = 'session expired, please log in again';
 
     if (accessToken && expiryTime) {
       const currentTime = Date.now();
-      if (currentTime > parseInt(expiryTime)) {
+      if (currentTime > parseInt(expiryTime, 10)) {
         dispatch(resetTokens());
         alert(notice);
       }
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -4,21 +4,21 @@ import { createSlice } from '@reduxjs/toolkit';
 import { signin, signup, getUsername } from './apiSlice';
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState: {
     username: null,
-    resource_owner: JSON.parse(localStorage.getItem("resource_owner")),
+    resource_owner: JSON.parse(localStorage.getItem('resource_owner')),
     loading: false,
     error: null,
     message: null,
   },
   reducers: {
     resetTokens: (state) => {
-      localStorage.removeItem("refresh_token");
-      localStorage.removeItem("resource_owner");
-      localStorage.removeItem("resource_name");
-      localStorage.removeItem("access_token");
-      localStorage.removeItem("expires_in"); //added this so that it removes the expires_in from local storage on logout.
+      localStorage.removeItem('refresh_token');
+      localStorage.removeItem('resource_owner');
+      localStorage.removeItem('resource_name');
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('expires_in'); // added this so that it removes the expires_in from local storage on logout.
       state.username = null;
       state.error = null;
       state.message = null;
@@ -41,14 +41,14 @@ const authSlice = createSlice({
       })
       .addCase(signup.fulfilled, (state, action) => {
         const { resource_owner, refresh_token, token } = action.payload;
-        localStorage.setItem("resource_owner", JSON.stringify(resource_owner));
-        localStorage.setItem("refresh_token", refresh_token);
-        localStorage.setItem("access_token", token);
-        localStorage.setItem("expires_in", Date.now() + 3600000); //added this, converts miliseconds to hours, one hour in this case.
+        localStorage.setItem('resource_owner', JSON.stringify(resource_owner));
+        localStorage.setItem('refresh_token', refresh_token);
+        localStorage.setItem('access_token', token);
+        localStorage.setItem('expires_in', Date.now() + 3600000); // added this, converts miliseconds to hours, one hour in this case.
         state.resource_owner = resource_owner;
         state.loading = false;
         state.error = null;
-        state.message = "Sign up successfull, please log into your account";
+        state.message = 'Sign up successfull, please log into your account';
       })
       .addCase(signin.pending, (state) => {
         state.loading = true;
@@ -59,16 +59,16 @@ const authSlice = createSlice({
       })
       .addCase(signin.fulfilled, (state, action) => {
         const { token, refresh_token, resource_owner } = action.payload;
-        localStorage.setItem("resource_owner", JSON.stringify(resource_owner));
-        localStorage.setItem("refresh_token", refresh_token);
-        localStorage.setItem("access_token", token);
-        localStorage.setItem("expires_in", Date.now() + 3600000); //added this converts miliseconds to hours, one hour in this case.
+        localStorage.setItem('resource_owner', JSON.stringify(resource_owner));
+        localStorage.setItem('refresh_token', refresh_token);
+        localStorage.setItem('access_token', token);
+        localStorage.setItem('expires_in', Date.now() + 3600000); // added this converts miliseconds to hours, one hour in this case.
         state.resource_owner = resource_owner;
         state.loading = false;
       })
       .addCase(getUsername.fulfilled, (state, action) => {
         const name = action.payload;
-        localStorage.setItem("resource_name", name);
+        localStorage.setItem('resource_name', name);
         state.username = name;
       });
   },
