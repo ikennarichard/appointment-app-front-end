d7ea2be36586f58af06cb7ad08061903179d1308
theabroadwolf <belalgamal00@gmail.com>
2023-08-28 07:29:43 +0300

    Add classes for car details page

--- src/components/cars/CarDetails.jsx
+++ src/components/cars/CarDetails.jsx
@@ -10,20 +10,25 @@ export default function CarDetails() {
   };
 
   return (
-    <section>
-      <h2 className="car_model">{state.car_model}</h2>
-      <img src={state.photo} alt={`a nice ${state.car_model}`} />
-      <p>{state.description}</p>
-      <b>{`Reservation Price: ${state.reservation_price}`}</b>
-      <Link
-        to="add_reservation"
-        state={details}
-      >
-        {
-          state.user_id === resourceOwner.id
-            ? '' : <button type="button"> Reserve </button>
-        }
-      </Link>
+    <section id="car-details-container" className="container">
+      <div className="row gap-4">
+        <div className="show-car-image-container">
+          <img src={state.photo} alt={`a nice ${state.car_model}`} className="img-fluid" />
+        </div>
+        <div className="d-flex flex-column gap-2">
+          <h1 className="display-5 fw-medium m-0">{state.car_model}</h1>
+          <div>
+            <span>Description: </span>
+            <span>{state.description}</span>
+          </div>
+          <b>{`Reservation Price: ${state.reservation_price}`}</b>
+          <div>
+            <Link to="add_reservation" state={details} className="car-details-btn">
+              {state.user_id === resourceOwner.id ? '' : 'Reserve'}
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
