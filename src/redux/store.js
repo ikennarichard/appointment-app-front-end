6843a365c1490b60365dc3914c811fc1e5e241bf
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-28 19:35:33 -1100

    Remove boilerplate files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import testReducer from './test/testSlice';
 import authReducer from './auth/authSlice';
 import carsReducer from './cars/carsSlice';
 import reservationsReducer from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
-    test: testReducer,
     auth: authReducer,
     cars: carsReducer,
     reservation: reservationsReducer,
