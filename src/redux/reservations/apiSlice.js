d0b5016146753d02177c05317cfb0510b9cc4394
theabroadwolf <belalgamal00@gmail.com>
2023-08-31 17:57:21 +0300

    Add delete reservation api request

--- src/redux/reservations/apiSlice.js
+++ src/redux/reservations/apiSlice.js
@@ -19,19 +19,49 @@ export const getReservations = createAsyncThunk('reservations/getAllReservations
   }
 });
 
-export const addReservation = createAsyncThunk('reservations/addReservation', async ({ userId, carId, details }) => {
-  try {
-    const response = await axios.post(`http://127.0.0.1:3000/users/${userId}/cars/${carId}/reservations`, details, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-      },
-    });
-    if (response.status === 201) {
-      return response.data;
+export const addReservation = createAsyncThunk(
+  'reservations/addReservation',
+  async ({ userId, carId, details }) => {
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:3000/users/${userId}/cars/${carId}/reservations`,
+        details,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          },
+        }
+      );
+      if (response.status === 201) {
+        return response.data;
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error ocuured while reserving car');
     }
-  } catch (error) {
-    console.error(error);
-    throw new Error('An error ocuured while reserving car');
   }
-});
+);
+
+export const deleteReservation = createAsyncThunk(
+  'reservations/deleteReservation',
+  async ({ userId, reservationId }) => {
+    try {
+      const response = await axios.delete(
+        `http://127.0.0.1:3000/users/${userId}/reservations/${reservationId}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          },
+        }
+      );
+      if (response.statusText === 'OK') {
+        return 'Reservation deleted successfully';
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error occured while deleting reservation');
+    }
+  }
+);
